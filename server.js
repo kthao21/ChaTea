135321933fab0215cd662d5932334f065d7d5af3
Nima Sherpa <nimasherpalol7@gmail.com>
2023-09-25 19:44:40 -0700

    merge

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const expbs = require('express-handlebars');
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('Sever is starting at port', 3001)
+});
