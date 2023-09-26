79db5552cfb1319c5e77e711953cf70947ca9412
Andrew Stave <andrewdstave@gmail.com>
2023-09-25 19:15:46 -0700

    added routes and JS

--- server.js
+++ server.js
@@ -1,9 +1,65 @@
 const express = require('express');
 const app = express();
 const expbs = require('express-handlebars');
+const path = require('path');
 
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+
+
+  res.render('home', { title: 'ChaTea' });
+});
+
+
+app.get('/', (req, res) => {
+    res.render('home'); //Displays home
+  });
+ 
+  //Displays login
+app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+
+  //Login form submission
+  app.post('/login', (req, res) => {
+  });
+
+
+  //Logout route
+  app.get('/logout', (req, res) => {
+  });
+ 
+  //Fetches user data
+  app.get('/profile', (req, res) => {
+    const userData = {};
+    res.render('profile', { userData });
+  });
+ 
+//Displays chat room
+app.get('/chat/:roomId', (req, res) => {
+    const roomId = req.params.roomId;
+    //Fetches chat room data for the id of said chat room
+    const chatData = {};
+    res.render('chat', { chatData });
+  });
+ 
+  //with sending messages
+  app.post('/chat/:roomId/send', (req, res) => {
+    const roomId = req.params.roomId;
+    const messageText = req.body.message;
+  });
+ 
 
 
 app.listen(3001, () => {
---
+++ views/js/login.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+
+
+  loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+
+    const email = document.getElementById("email-login").value;
+    const password = document.getElementById("password-login").value;
+
+
+ 
+      try {
+        //VERIES AUTHENTICIT AND ALL THAT
+        const response = await fetch('/login', {    //<------------------------- dont forget this probably not done
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        });
+ 
+        if (response.ok) {
+
+
+            window.location.href = "/profile";
+          } else if (response.status === 401) {
+
+
+            alert("Username or password incorrect.");
+          } else {
+            console.error("Server error:", response.status);
+            alert("An error occurred from inside.");
+          }
+        } catch (error) {
+          console.error("An error has occurred", error);
+          alert("An error has occurred.");
+        }
+      });
+    });
