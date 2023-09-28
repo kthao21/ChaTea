464b5ac6f42b21a017dfae4100f9f285dcb59a32
Justin Bobrick <j.bobrick@outlook.com>
2023-09-27 18:40:32 -0700

    conflicts

--- socket.js
+++ socket.js
@@ -1,6 +1,12 @@
-const io = require('socket.io')(3001);
-const express = require('express');
+const express = require("express");
+const app = express();
+const path = require('path');
 
-io.on('connection', socket => {
-    console.log(socket.id)
-})
+const http = require("http").Server(app);
+const port = process.env.PORT || 3001
+
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
